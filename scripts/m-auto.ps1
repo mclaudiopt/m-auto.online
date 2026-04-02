36ef820b541316ed40b1c8ad61644f94a5fb1019
mclaudiopt <marcelo.claudio@gmail.com>
2026-04-02 16:08:21 +0100

    version: bump to 1.1 [2026-04-02 16:08:21]

--- scripts/m-auto.ps1
+++ scripts/m-auto.ps1
@@ -1,7 +1,7 @@
 ﻿# M-Auto Online - Remote Installer Launcher
 # Usage: irm https://m-auto.online/scripts/m-auto.ps1 | iex
 
-$VERSION  = "1.1 [2026-04-02 15:58:43]"
+$VERSION  = "1.1 [2026-04-02 16:08:21]"
 $BASE_URL = "https://m-auto.online/scripts"
 $e = [char]27
 
@@ -295,6 +295,7 @@ while ($true) {
         default { Write-Warn "Opcao invalida." ; Start-Sleep -Milliseconds 600 }
     }
 }
+
 
 
 
